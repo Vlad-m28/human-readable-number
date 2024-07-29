18967bf5124310fcffd8d013cc3546a25b68a25d
Vlad <vlad.murylyv@mail.ru>
2024-07-29 23:54:58 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+if (number == 0) return "zero";
+
+if (number > 99) {
+    return (ones[Math.floor(number / 100)] + " hundred " + convert_tens(number % 100)).trim();
+} else {
+    return convert_tens(number);
+}
+
+function convert_tens(number) {
+  if (number < 10) return ones[number];
+  else if (number >= 10 && number < 20) return teens[number - 10];
+  else {
+    return (tens[Math.floor(number / 10)] + " " + ones[number % 10]).trim();
+  }
+}
+
 }
